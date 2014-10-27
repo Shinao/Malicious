e549e4d33b1c27518a91fde8201383b646533427
unknown <rmonnerat.pro@gmail.com>
2014-10-27 19:26:36 +0100

    Iterate Imported Functions

--- get_info_file.asm
+++ get_info_file.asm
@@ -143,7 +143,7 @@ mov	eax, [eax]
 sub	edx, eax
 mov	[DELTA OffsetIAT], edx
 notIAT:
-add	esi, 028h
+add	esi, 028h ; Keep End Header Section
 loop	Loop_SectionHeader
 
 
@@ -158,7 +158,7 @@ sub	edi, eax
 mov	eax, [DELTA PeFileMap]
 add	edi, eax ; IAT
 
-; Iterate on all IAT
+; Iterate on all IAT Modules
 iterateIAT:
 mov	edx, edi
 add	edx, 0Ch ; Dll Name
@@ -176,6 +176,33 @@ push	edx
 push	0
 call	[DELTA pMessageBox]
 popa
+
+; Iterate on all Imported Functions
+mov	edx, edi
+mov	edx, [edx]
+add	edx, eax
+sub	edx, ecx
+iterateFunc:
+mov	ebx, [edx]
+cmp	ebx, 0
+je	endIterateFunc
+add	ebx, eax
+sub	ebx, ecx
+add	ebx, 2 ; TODO - WHY !?
+
+; pusha
+; push	0
+; push	ebx
+; push	ebx
+; push	0
+; call	[DELTA pMessageBox]
+; popa
+
+add	edx, 4
+jmp	iterateFunc
+
+endIterateFunc:
+
 
 add	edi, sizeof (IMAGE_IMPORT_DESCRIPTOR)
 jmp	iterateIAT
