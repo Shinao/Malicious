06f17c163a0a5225ccf29f4c85daac956059cce1
unknown <rmonnerat.pro@gmail.com>
2014-11-10 18:49:07 +0100

    Clean

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ set NAME=inject
 if exist %NAME%.obj del %NAME%.obj
 if exist %NAME%.exe del %NAME%.exe
 
-\masm32\bin\ml /c /coff /nologo %NAME%.asm
+\masm32\bin\ml /c /coff /nologo src/%NAME%.asm
 \masm32\bin\link /SUBSYSTEM:WINDOWS %NAME%.obj > nul
 
 :: DEBUG
